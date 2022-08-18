652efbb0f7935603dcf7f14ef58545a34239430e
Dmitry <orionvega5@gmail.com>
2022-08-19 00:26:27 +0300

    Add Task035 and Task037

---
+++ Task035/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] ArrayCreation(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(1, 124);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int FindElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+            {
+                count++;
+            }
+    }
+    return count;
+}
+
+int[] myarr = ArrayCreation(123);
+PrintArray(myarr);
+int count = FindElements(myarr);
+Console.WriteLine(count);
---
+++ Task037/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] ArrayCreation(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(1, 6);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] Multiplication(int[] arr)
+{
+    int len = arr.Length;
+    int new_len = 0;
+    if (len % 2 == 0) new_len = len / 2;
+    else new_len = len / 2 + 1;
+    int[] new_array = new int[new_len];
+    
+    for (int i = 0; i < new_len ; i++)
+    {
+        new_array[i] = arr[i] * arr[len - 1 - i];
+    }
+    return new_array;
+}
+Console.Write("Введите желаемую длину массива в виде целого числа: ");
+int len_array = Convert.ToInt32(Console.ReadLine());
+
+int[] myarr = ArrayCreation(len_array);
+PrintArray(myarr);
+int[] new_array = Multiplication(myarr);
+PrintArray(new_array);
